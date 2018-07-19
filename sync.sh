d3ed5f9334d4189d8b0f6ceb152b5b3e06285f8b
Justin Lee <tlee@gadventures.com>
2018-07-19 10:34:32 -0400

    $message

--- sync.sh
+++ sync.sh
@@ -3,10 +3,25 @@
 THIS_DIR=`pwd`
 
 # basic configs
+echo Updating vimrc
 ln -sf $THIS_DIR/.vimrc ~/.vimrc
+
+echo Updating tmux conf
 ln -sf $THIS_DIR/.tmux.conf ~/.tmux.conf
+
+echo Updating zshrc
 ln -sf $THIS_DIR/.zshrc ~/.zshrc
 
-git add .
-git commit -m "Setting change"
-git push
+echo Detecting Git changes
+if git diff-index --quiet HEAD --; then
+    # No changes
+    echo No Changes
+else
+    # Changes
+    read -p 'Changes Detected. Push changes to Git?(y/N): ' confirm && [[ $confirm == [y] ]] || (echo Command Aborted; exit 1)
+
+    git add .
+    read -r -p 'Commit Message: ' message
+    git commit -m '$message'
+    git push
+fi
